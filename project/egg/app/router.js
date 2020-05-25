1050b5ff6ef07b7ff4a3aa1cfdd3db1851eb26c7
pilar <601991736@qq.com>
2020-05-25 18:10:32 +0800

    mysql

--- project/egg/app/router.js
+++ project/egg/app/router.js
@@ -7,7 +7,8 @@ module.exports = async app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
-  console.log(app.mysql);
-  
-  
+  console.log(98876);
+
+  let res = await app.mysql.select('puser', '*')
+  console.log(res);
 };
--- project/egg/config/config.default.js
+++ project/egg/config/config.default.js
@@ -12,6 +12,26 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
+  config.mysql =  {
+    // database configuration
+    client: {
+      // host
+      host: '127.0.0.1',
+      // port
+      port: '3307',
+      // username
+      user: 'root',
+      // password
+      password: '12345678',
+      // database
+      database: 'test',    
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  }
+
   
 
   // use for cookie sign key, should change to your own and keep security
@@ -29,24 +49,4 @@ module.exports = appInfo => {
     ...config,
     ...userConfig,
   };
-};
-
-exports.mysql = {
-  // database configuration
-  client: {
-    // host
-    host: '127.0.0.1',
-    // port
-    port: '3306',
-    // username
-    user: 'mytest',
-    // password
-    password: '123456',
-    // database
-    database: 'test',    
-  },
-  // load into app, default is open
-  app: true,
-  // load into agent, default is close
-  agent: false,
 };
